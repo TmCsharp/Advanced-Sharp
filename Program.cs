a74f8f249d6b8c67ac1bc75a4baf4790e316fcc0
TmCsharp <HugonunAmk007>
2024-11-17 19:01:54 +0200

    Some more codes

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "Golf";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
