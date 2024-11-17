102a5eb71834ad40a80f593264ac370875b4d7fe
TM <115100205+TmCsharp@users.noreply.github.com>
2024-11-17 20:00:49 +0200

    Update Car.cs

--- Car.cs
+++ Car.cs
@@ -8,7 +8,6 @@ namespace CarManufacturer
         private string make;
         private string model;
         private int year;
-
        
         public string Make { get { return make; } set { make = value; } }
         public string Model { get { return model; } set { model = value; } }
