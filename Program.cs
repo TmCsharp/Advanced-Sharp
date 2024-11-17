14c5776ab7708d32d90915a855a451d0a05976b6
TM <115100205+TmCsharp@users.noreply.github.com>
2024-11-17 20:01:44 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,11 +6,9 @@ namespace CarManufacturer
         static void Main()
         {
             Car car = new Car();
-
             car.Make = "VW";
             car.Model = "Golf";
             car.Year = 1992;
-
             Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
         }
     }
